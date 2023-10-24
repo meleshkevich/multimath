c8815ae1ef0ed90068b6ced3254bbcc63d8c7f4f
Ilja M <i.meleshkevich@gmail.com>
2023-10-24 23:58:48 +0200

    refactor with classes

--- app/app.ts
+++ app/app.ts
@@ -1,49 +1,5 @@
-function startGame() {
-  //starting new game
-  let playerName: string | undefined = getInputValue("playername");
-  logPlayer(playerName);
+/// <reference path="player.ts" />
+/// <reference path="game.ts" />
 
-  postScore(90, playerName);
-  postScore(-90, playerName);
-}
-
-function logPlayer(name: string = "MultiMath Player"): void {
-  console.log(`New game starting for palayer: ${name}`);
-}
-function getInputValue(elementID: string): string | undefined {
-  const inputElement: HTMLInputElement = <HTMLInputElement>(
-    document.getElementById(elementID)
-  );
-
-  if (inputElement.value === "") {
-    return undefined;
-  } else {
-    return inputElement.value;
-  }
-}
-
-function postScore(
-  score: number,
-  playerName: string = "MultiMath Player"
-): void {
-  let logger: (value: string) => void;
-
-  if (score < 0) {
-    logger = logError;
-  } else {
-    logger = logMessage;
-  }
-
-  const scoreElement: HTMLElement | null =
-    document.getElementById("postedScores");
-  scoreElement!.innerText = `${score} - ${playerName}`;
-
-  logger(`${score}`);
-}
-document.getElementById("startGame")!.addEventListener("click", startGame);
-
-const logMessage = (message: string) => console.log(message);
-
-function logError(err: string): void {
-  console.error(err);
-}
+let newGame: Game;
+// 6:30
---
+++ js/app.js
@@ -0,0 +1,44 @@
+class Player {
+    formatName() {
+        return this.name.toUpperCase();
+    }
+}
+function startGame() {
+    let playerName = getInputValue("playername");
+    logPlayer(playerName);
+    postScore(90, playerName);
+    postScore(-90, playerName);
+}
+function logPlayer(name = "MultiMath Player") {
+    console.log(`New game starting for palayer: ${name}`);
+}
+function getInputValue(elementID) {
+    const inputElement = (document.getElementById(elementID));
+    if (inputElement.value === "") {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+function postScore(score, playerName = "MultiMath Player") {
+    let logger;
+    if (score < 0) {
+        logger = logError;
+    }
+    else {
+        logger = logMessage;
+    }
+    const scoreElement = document.getElementById("postedScores");
+    scoreElement.innerText = `${score} - ${playerName}`;
+    logger(`${score}`);
+}
+document.getElementById("startGame").addEventListener("click", startGame);
+const logMessage = (message) => console.log(message);
+function logError(err) {
+    console.error(err);
+}
+const firstPlayer = new Player();
+firstPlayer.name = "Lanier";
+console.log(firstPlayer.formatName());
+//# sourceMappingURL=app.js.map
